ba4709c33328d61fff5cd125c59dfcc3b4b81670
Steven Wing <steven.wing82@gmail.com>
2019-05-29 01:31:47 +0000

    created basic functions in game.js

---
+++ game.js
@@ -0,0 +1,38 @@
+const generateBoard = function(boardSize) {
+
+}
+
+
+
+const newGame = function(options, opponent) {
+  let sizeOfBoard = 10;
+  if (options.board) {
+    sizeOfBoard = options.board;
+  }
+
+  generateBoard(sizeOfBoard);
+
+
+}
+
+const placeShips = function() {
+
+}
+
+const playTurn = function() {
+
+}
+
+
+// AI player
+const placeShipsAI = function(sizeOfBoard) {
+
+}
+const playTurnAI = function() {
+
+}
+
+// Misc items
+const showLeaderBoard = function() {
+
+}
